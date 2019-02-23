5e07c305cb2748dabbd06f261bee0ff93b537eba
Shawn Wilson <shawn.wilson137@gmail.com>
2019-02-23 19:01:06 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { Kehlani: "Distraction", Mac_Ayres: "Easy"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[Ella_Mai] = Anymore
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[Mac_Ayres] = Easy
+  return playlist
+}
